0ade694e7cc9d0a93004c92368ee68bbd332f830
sssaaammmuuurrraaaiii <nikitabolotskijy.maier@yandex.ru>
2023-10-24 12:43:44 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./../styles/Header.css";
+
+
+function Header() {
+    return (
+        <div className="Header">
+            <h1>Прогнозирование фондовых индексов</h1>
+        </div>
+    )
+}
+
+export default Header
