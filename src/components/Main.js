68dee371d95b919c040e03baa7a95168c0ec2a2b
sssaaammmuuurrraaaiii <nikitabolotskijy.maier@yandex.ru>
2023-10-29 03:55:47 +0300

    second day coding

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,29 +4,29 @@ import "./../styles/Main.scss";
 function Main() {
     return (
         <div className="Main">
-            <form name="dataOutputSettings" className="UserForm">
+            <form name="dataOutputSettings" className="flexbox_form">
                 <div>
-                    <label htmlFor="indexCode" className="FormLabel_FormInputText">Код индекса</label>
+                    <label htmlFor="indexCode">Код индекса</label>
                 </div>
                 <div>
-                    <input id="indexCode" name="indexCode" className="FormInputText"></input>
+                    <input id="indexCode" name="indexCode" type="text" className="form-inpute-value"></input>
                 </div>
                 <div>
-                    <label htmlFor="startDate" className="FormLabel_FormInputStartDate">С</label>
-                    <input id="startDate" name="startDate" type="date" className="FormInputDate"></input>
+                    <label htmlFor="startDate">С</label>
+                    <input id="startDate" name="startDate" type="date" className="form-input-date"></input>
                 </div>
                 <div>
-                    <label htmlFor="endDate" className="FormLabel_FormInputEndDate">По</label>
-                    <input id="endDate" name="endDate" type="date" className="FormInputDate"></input>
+                    <label htmlFor="endDate" className="form-label">По</label>
+                    <input id="endDate" name="endDate" type="date" className="form-input-date"></input>
                 </div>
                 <div>
-                    <label htmlFor="percentage" className="FormLabel_FormInputText">Процент для рассчета<br></br>снижения</label>
+                    <label htmlFor="percentage">Процент для рассчета<br></br>снижения</label>
                 </div>
                 <div>
-                    <input id="percentage" name="percentage" className="FormInputText"></input>
+                    <input id="percentage" name="percentage" type="text" className="form-inpute-value"></input>
                 </div>
                 <div>
-                    <button id="calculate" name="сalculate" className="FormButton">Рассчитать</button>
+                    <button id="calculate" name="сalculate" className="form-button-accept">Рассчитать</button>
                 </div>
             </form>
         </div>
